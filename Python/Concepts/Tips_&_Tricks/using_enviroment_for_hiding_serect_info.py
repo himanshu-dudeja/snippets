3cd7a92680524aeaa0d3bc72a486dc317ac1fe92
Himanshu Dudeja <himanshu.dudeja956@gmail.com>
2020-07-11 16:12:59 +0530

    Way to Add enviroment varibales in Windows also mentioned in comments

--- Python/Concepts/Tips_&_Tricks/using_enviroment_for_hiding_serect_info.py
+++ Python/Concepts/Tips_&_Tricks/using_enviroment_for_hiding_serect_info.py
@@ -1,8 +1,10 @@
 
 # For Linux/Mac edit the .bash_profile or .bashrc or .bash_aliases file and add ->
-
 # export DB_USER="db_user_name";
 # export DB_PASS="pass123";
+
+# For windows - Control Panel -> System and security -> system -> Advanced System settings -> Enviroment Variables button (Add User Variables)
+
 
 import os
 
