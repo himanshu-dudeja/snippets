8a4867b4b1837a588b5f5ea24cbab4927c3a6e2a
Himanshu Dudeja <himanshu.dudeja956@gmail.com>
2021-01-17 17:13:30 +0530

    Added Merge sort code

---
+++ Python/DSA/Sorting/6.merge.py
@@ -0,0 +1,31 @@
+def merge(data):
+    if len(data) > 1:
+        mid = len(data) // 2
+        left = data[:mid]
+        right = data[mid:]
+
+        merge(left)
+        merge(right)
+
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                data[k] = left[i]
+                i += 1
+            else:
+                data[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            data[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            data[k] = right[j]
+            j += 1
+            k += 1
+
+
+mylist = [15, 5, 24, 8, 1, 3, 16, 10, 20]
+merge(mylist)
+print(mylist)
